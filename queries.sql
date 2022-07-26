6cdbaad0889245ad746f00024f1f9947fbad3b6c
AlineCantalice <alinedecantalice@gmail.com>
2022-07-26 20:06:58 -0300

    query bonus

--- queries.sql
+++ queries.sql
@@ -16,3 +16,10 @@ select u.id, u.name, c.name as course, s.name as school, ed."endDate" from users
 -- Exercicio 4
 
 select u.id, u.name, r.name as role, c.name as company, e."startDate" from users u join experiences e on e."userId"=u.id join roles r on e."roleId"=r.id join companies c on e."companyId"=c.id where u.id=50 and e."endDate" is null;
+
+
+-- Bonus
+
+select s.id, s.name as school, co.name as course, c.name as company, r.name as role from jobs j join roles r on j."roleId"=r.id join companies c on j."companyId"=c.id join applicants a on j.id=a."jobId" join users u on a."userId"=u.id join educations e on u.id=e."userId" join schools s on e."schoolId"=s.id join courses co on co.id=e."courseId" where j.active=true and c.id=10 and r.name='Software Engineer' order by s.id;
+
+
